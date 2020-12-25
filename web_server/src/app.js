0725154d12cd4cee4659908ae15d453504f8c8f8
Abhishek <abhishekt2024@gmail.com>
2020-12-25 21:20:18 +0530

    added web_server

---
+++ web_server/src/app.js
@@ -0,0 +1,65 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+const dirViews = path.join(__dirname,'../template/views')
+const dirPartials = path.join(__dirname,'../template/partials')
+const dirPublic = path.join(__dirname,'../public')
+
+//setup handlebars
+app.set('views', dirViews)
+app.set('view engine', 'hbs')
+hbs.registerPartials(dirPartials)
+
+app.use(express.static(dirPublic))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:"Weather",
+        name:"JUZo"
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:"Help",
+        name:"JUZo"
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:"About Me",
+        name:"JUZo"
+    })
+})
+
+app.get('/weather',(req,res)=>{
+    res.send({
+        location: "Mumbai",
+        forecast: "100"
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:"Page not Found",
+        errorContent:"Try help section",
+        name:"JUZo"
+    })
+})
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:"Page not Found",
+        errorContent:"Try other Pages",
+        name:"JUZo"
+    })
+})
+
+app.listen(3000,(error)=>{
+    if(error)
+        console.log(error)
+    else
+        console.log('Server is up on port 3000')
+})
